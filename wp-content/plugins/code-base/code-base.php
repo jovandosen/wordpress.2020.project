c20a261721874b269612b7fe79a5f9afebe162d2
jovandosen <jovandosen1994@gmail.com>
2020-04-07 13:14:04 +0200

    Top level menu example added

--- wp-content/plugins/code-base/code-base.php
+++ wp-content/plugins/code-base/code-base.php
@@ -57,4 +57,82 @@
 
  register_uninstall_hook(__FILE__, 'code_base_uninstall');
 
+ function wporg_options_page_html() 
+ {
+    ?>
+    <div class="wrap">
+      <h1><?php esc_html( get_admin_page_title() ); ?></h1>
+      <form action="options.php" method="post">
+        <?php
+        // output security fields for the registered setting "wporg_options"
+        settings_fields( 'wporg_options' );
+        // output setting sections and their fields
+        // (sections are registered for "wporg", each field is registered to a specific section)
+        do_settings_sections( 'wporg' );
+        // output save settings button
+        submit_button( 'Save Settings' );
+        ?>
+      </form>
+    </div>
+    <?php
+ }
+
+ add_action( 'admin_menu', 'wporg_options_page' );
+
+ function wporg_options_page() 
+ {
+    add_menu_page(
+        'WPOrg',
+        'WPOrg Options',
+        'manage_options',
+        'wporg',
+        'wporg_options_page_html',
+        '',
+        20
+    );
+ }
+
+ function code_base_page()
+ {
+ 	?>
+ 	<div class="wrap">
+ 		<h1>Code Base Page</h1>
+ 		<p>Learn Wordpress.</p>
+ 		<form method="post" action="<?php menu_page_url( 'code-base' ) ?>">
+ 			<input type="text" name="data" autocomplete="off">
+ 			<input type="submit" name="send" value="SEND">
+ 			<input type="hidden" name="check" value="<?php echo wp_create_nonce('check'); ?>">
+ 		</form>
+ 	</div>
+ 	<?php
+ }
+
+ add_action('admin_menu', 'code_base_options');
+
+ function code_base_options()
+ {
+ 	$hookName = add_menu_page(
+ 		'Code Base',
+ 		'Code Base Options',
+ 		'manage_options',
+ 		'code-base',
+ 		'code_base_page',
+ 		'',
+ 		30
+ 	);
+
+ 	add_action( 'load-' . $hookName, 'code_base_page_submit' );
+ }
+
+ function code_base_page_submit()
+ {
+ 	// runs on form submit
+ 	if( 'POST' === $_SERVER['REQUEST_METHOD'] && wp_verify_nonce($_POST['check'], 'check') ){
+ 		if( !empty($_POST['data']) ){
+ 			$data = $_POST['data'];
+ 			echo "You have submitted: " . $data;
+ 		}
+ 	}
+ }
+
 ?>

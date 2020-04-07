281e0c28e749a8b97e2cb9560836baeb1f3b13a3
jovandosen <jovandosen1994@gmail.com>
2020-04-07 17:35:41 +0200

    Settings API Examples added

--- wp-content/plugins/code-base/code-base.php
+++ wp-content/plugins/code-base/code-base.php
@@ -287,4 +287,89 @@
 
  // end of short code examples
 
+ // Settings API
+
+ function wporg_settings_init()
+ {
+    // register a new setting for "reading" page
+    register_setting('reading', 'wporg_setting_name');
+ 
+    // register a new section in the "reading" page
+    add_settings_section(
+        'wporg_settings_section',
+        'WPOrg Settings Section',
+        'wporg_settings_section_cb',
+        'reading'
+    );
+ 
+    // register a new field in the "wporg_settings_section" section, inside the "reading" page
+    add_settings_field(
+        'wporg_settings_field',
+        'WPOrg Setting',
+        'wporg_settings_field_cb',
+        'reading',
+        'wporg_settings_section'
+    );
+ }
+
+ add_action('admin_init', 'wporg_settings_init');
+
+ // section content cb
+ function wporg_settings_section_cb()
+ {
+    echo '<p>WPOrg Section Introduction.</p>';
+ }
+ 
+ // field content cb
+ function wporg_settings_field_cb()
+ {
+    // get the value of the setting we've registered with register_setting()
+    $setting = get_option('wporg_setting_name');
+    // output the field
+    ?>
+    <input type="text" name="wporg_setting_name" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
+    <?php
+ }
+
+ //
+
+ function add_example_data()
+ {
+ 	register_setting('reading', 'example_setting_name');
+
+ 	add_settings_section(
+        'example_settings_section',
+        'Example Settings Section',
+        'example_settings_section_cb',
+        'reading'
+    );
+
+    add_settings_field(
+        'example_settings_field',
+        'Example Setting',
+        'example_settings_field_cb',
+        'reading',
+        'example_settings_section'
+    );
+ }
+
+ add_action('admin_init', 'add_example_data');
+
+ function example_settings_section_cb()
+ {
+    echo '<p>Example Section Introduction.</p>';
+ }
+
+ function example_settings_field_cb()
+ {
+    // get the value of the setting we've registered with register_setting()
+    $setting = get_option('example_setting_name');
+    // output the field
+    ?>
+    <input type="text" name="example_setting_name" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
+    <?php
+ }
+
+ // end of settings API
+
 ?>

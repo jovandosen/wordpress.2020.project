5672197c80c01eba9e59f1130547f7dfc54a3266
jovandosen <jovandosen1994@gmail.com>
2020-04-07 15:44:40 +0200

    Shortcode examples added

--- wp-content/plugins/code-base/code-base.php
+++ wp-content/plugins/code-base/code-base.php
@@ -223,4 +223,64 @@
  	}
  }
 
+ // short code examples
+
+ function runShortCodeFunction($atts = [], $content = null, $tag = '')
+ {
+ 	return 'Shortcode text to display.';
+ }
+
+ function code_base_shortcode()
+ {
+ 	add_shortcode('test', 'runShortCodeFunction');
+ }
+
+ add_action('init', 'code_base_shortcode');
+
+ //
+
+ function runExampleShortcode($atts = [], $content = null, $tag = '')
+ {
+ 	// normalize attribute keys, lowercase
+    $atts = array_change_key_case((array)$atts, CASE_LOWER);
+ 
+    // override default attributes with user attributes
+    $wporg_atts = shortcode_atts([
+                                     'title' => 'WordPress.org',
+                                 ], $atts, $tag);
+ 
+    // start output
+    $o = '';
+ 
+    // start box
+    $o .= '<div class="wporg-box">';
+ 
+    // title
+    $o .= '<h2>' . esc_html__($wporg_atts['title'], 'wporg') . '</h2>';
+ 
+    // enclosing tags
+    if (!is_null($content)) {
+        // secure output by executing the_content filter hook on $content
+        $o .= apply_filters('the_content', $content);
+ 
+        // run shortcode parser recursively
+        $o .= do_shortcode($content);
+    }
+ 
+    // end box
+    $o .= '</div>';
+ 
+    // return output
+    return $o;
+ }
+
+ function code_base_example_shortcode()
+ {
+ 	add_shortcode('dev', 'runExampleShortcode');
+ }
+
+ add_action('init', 'code_base_example_shortcode');
+
+ // end of short code examples
+
 ?>

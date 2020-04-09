b217e7715d3dda4a2d1b09d9c2c9a1e13e88cf6d
jovandosen <jovandosen1994@gmail.com>
2020-04-09 11:22:26 +0200

    Users CRUD updated

--- wp-content/plugins/users-logic.php
+++ wp-content/plugins/users-logic.php
@@ -73,3 +73,59 @@ function insertTestUser()
 }
 
 add_action('init', 'insertTestUser');
+
+function insertExampleUser()
+{
+	$username = "Petar";
+	$password = "petar";
+	$website = "http://petartest.com";
+	$email = "petar@gmail.com";
+
+	$user_data = [
+		'user_login' => $username,
+		'user_pass' => $password,
+		'user_url' => $website,
+		'user_email' => $email
+	];
+
+	$user_id = username_exists($username);
+
+	if( !$user_id && email_exists($email) === false ){
+		$user_id = wp_insert_user($user_data);
+		if( !is_wp_error($user_id) ){
+			echo 'User created: ' . $user_id;
+		}
+	}
+}
+
+add_action('init', 'insertExampleUser');
+
+function updateUser()
+{
+	$user_id = 5;
+	$website = 'http://petardev.com';
+	$new_email = "petar123@gmail.com";
+ 
+	$user_id = wp_update_user([
+        'ID'       => $user_id,
+        'user_url' => $website,
+        'user_email' => $new_email,
+    ]);
+ 
+	if (is_wp_error($user_id)) {
+	    // error
+	} else {
+	    // success
+	}
+}
+
+// add_action('init', 'updateUser');
+
+function deleteUser()
+{
+	$user_id = 5;
+	$reassign = null;
+	wp_delete_user($user_id, $reassign);
+}
+
+// add_action('init', 'deleteUser');

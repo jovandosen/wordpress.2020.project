62d5eaf2bf23613e84ca5c5889e38ad92d5e9d7a
jovandosen <jovandosen1994@gmail.com>
2020-04-10 12:35:57 +0200

    Cron example two added

--- wp-content/plugins/cron-example-one/cron-example-one.php
+++ wp-content/plugins/cron-example-one/cron-example-one.php
@@ -1,10 +1,14 @@
 <?php
 /*
-Plugin Name: Cron Example One
+* Plugin Name: Cron Example One
+* Description: This plugin prints all scheduled tasks by Cron job
+* Version: 1.0
+* Author: Jovan Dosen
+* Author URI: http://jovan-dosen.com
 */
 
 // This function prints all cron jobs
 function bl_print_tasks() {
     echo '<pre>'; print_r( _get_cron_array() ); echo '</pre>';
 }
-// bl_print_tasks();
+//bl_print_tasks();
---
+++ wp-content/plugins/cron-example-two/cron-example-two.php
@@ -0,0 +1,38 @@
+<?php
+/*
+* Plugin Name: Cron Example Two
+* Description: A short example showing how to work with CRON jobs
+* Version: 1.0
+* Author: Jovan Dosen
+* Author URI: http://jovan-dosen.com
+*/
+
+add_filter( 'cron_schedules', 'example_add_cron_interval' );
+
+function example_add_cron_interval( $schedules ) 
+{ 
+    $schedules['five_seconds'] = array(
+        'interval' => 5,
+        'display'  => esc_html__( 'Every Five Seconds' ), );
+    return $schedules;
+}
+
+add_action( 'bl_cron_hook', 'bl_cron_exec' );
+
+function bl_cron_exec()
+{
+	echo "Well and good.";
+}
+
+if ( ! wp_next_scheduled( 'bl_cron_hook' ) ) {
+    wp_schedule_event( time(), 'five_seconds', 'bl_cron_hook' );
+}
+
+// Upon deactivation
+register_deactivation_hook( __FILE__, 'bl_deactivate' ); 
+ 
+function bl_deactivate() 
+{
+    $timestamp = wp_next_scheduled( 'bl_cron_hook' );
+    wp_unschedule_event( $timestamp, 'bl_cron_hook' );
+}

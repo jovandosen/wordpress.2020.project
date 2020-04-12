44da38cbca1fbc137a4f3ba6f3c5c8ea7c9d3904
jovandosen <jovandosen1994@gmail.com>
2020-04-12 14:33:57 +0200

    Comments template added

--- wp-content/themes/example/single-events.php
+++ wp-content/themes/example/single-events.php
@@ -1,3 +1,15 @@
 <?php get_header(); ?>
 <h1>Single event</h1>
+
+<?php
+	
+	// If comments are open or we have at least one comment, load up the comment template.
+	if( comments_open() || get_comments_number() ){
+		comments_template();
+	} 
+
+?>
+
+<?php comment_form(); ?>
+
 <?php get_footer(); ?>

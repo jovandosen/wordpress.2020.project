36f7d314545b3b131608aa857d28898b446a9e1c
jovandosen <jovandosen1994@gmail.com>
2020-04-13 12:49:55 +0200

    Comments logic updated for Single Events

--- wp-content/themes/example/comments.php
+++ wp-content/themes/example/comments.php
@@ -4,20 +4,39 @@ if( post_password_required() ){
 	return;
 }
 
+// get post ID outside loop
+$postID = get_queried_object_id(); 
+
 //Get only the approved comments
 $args = array(
-    'status' => 'approve'
+    'status' => 'approve',
+    'post_id' => $postID
 );
  
 // The comment Query
 $comments_query = new WP_Comment_Query;
 $comments = $comments_query->query( $args );
- 
-// Comment Loop
-if ( $comments ) {
- 	foreach ( $comments as $comment ) {
- 		echo '<p>' . $comment->comment_content . '</p>';
- 	}
-} else {
- 	echo 'No comments found.';
-}
+
+?>
+
+<?php if( $comments ): ?>
+	<div class="comments-section">
+		<hr>
+		<h4>Comments Section:</h4>
+		<hr>
+		<?php foreach( $comments as $comment ): ?>
+			<div class="comment-data">
+				<p><?php echo 'Author: ' . $comment->comment_author; ?></p>
+				<p><?php echo 'Comment: ' . $comment->comment_content; ?></p>
+				<p><?php echo 'Created: ' . $comment->comment_date; ?></p>
+			</div>
+		<?php endforeach; ?>	
+	</div>
+<?php else: ?>
+	<div class="comments-section">
+		<hr>
+		<h4>Comments Section:</h4>
+		<hr>
+		<p><?php echo 'No comments found.'; ?></p>
+	</div>	
+<?php endif; ?>	 
--- wp-content/themes/example/single-events.php
+++ wp-content/themes/example/single-events.php
@@ -5,12 +5,10 @@
 <?php get_template_part('parts/content', 'event'); ?>
 
 <?php
-	
 	// If comments are open or we have at least one comment, load up the comment template.
-	/*if( comments_open() || get_comments_number() ){
+	if( comments_open() || get_comments_number() ){
 		comments_template();
-	}*/ 
-
+	}
 ?>
 
 <?php //comment_form(); ?>
